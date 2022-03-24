53cefa6d79c85244b33a8bcda59c08fcb44104da
samsonkemp <kem0002@nhs.vic.edu.au>
2022-03-24 11:48:18 +1100

    Commit Files

---
+++ Books Profit CSV/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Books_Profit_CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datagridBooks = new System.Windows.Forms.DataGridView();
+            this.TextBook = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Seller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Purchaser = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PurchasePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Profit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCalcProfit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datagridBooks
+            // 
+            this.datagridBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TextBook,
+            this.Subject,
+            this.Seller,
+            this.Purchaser,
+            this.PurchasePrice,
+            this.SalePrice,
+            this.Profit});
+            this.datagridBooks.Location = new System.Drawing.Point(28, 35);
+            this.datagridBooks.Name = "datagridBooks";
+            this.datagridBooks.Size = new System.Drawing.Size(745, 403);
+            this.datagridBooks.TabIndex = 0;
+            this.datagridBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridBooks_CellContentClick);
+            // 
+            // TextBook
+            // 
+            this.TextBook.HeaderText = "TextBook";
+            this.TextBook.Name = "TextBook";
+            // 
+            // Subject
+            // 
+            this.Subject.HeaderText = "Subject";
+            this.Subject.Name = "Subject";
+            // 
+            // Seller
+            // 
+            this.Seller.HeaderText = "Seller";
+            this.Seller.Name = "Seller";
+            // 
+            // Purchaser
+            // 
+            this.Purchaser.HeaderText = "Purchaser";
+            this.Purchaser.Name = "Purchaser";
+            // 
+            // PurchasePrice
+            // 
+            this.PurchasePrice.HeaderText = "Purchase Price";
+            this.PurchasePrice.Name = "PurchasePrice";
+            // 
+            // SalePrice
+            // 
+            this.SalePrice.HeaderText = "Sale Price";
+            this.SalePrice.Name = "SalePrice";
+            // 
+            // Profit
+            // 
+            this.Profit.HeaderText = "Profit";
+            this.Profit.Name = "Profit";
+            // 
+            // btnCalcProfit
+            // 
+            this.btnCalcProfit.Location = new System.Drawing.Point(28, 6);
+            this.btnCalcProfit.Name = "btnCalcProfit";
+            this.btnCalcProfit.Size = new System.Drawing.Size(138, 23);
+            this.btnCalcProfit.TabIndex = 1;
+            this.btnCalcProfit.Text = "Calculate Profit";
+            this.btnCalcProfit.UseVisualStyleBackColor = true;
+            this.btnCalcProfit.Click += new System.EventHandler(this.btnCalcProfit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcProfit);
+            this.Controls.Add(this.datagridBooks);
+            this.Name = "Form1";
+            this.Text = "TextBooks";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datagridBooks)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView datagridBooks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TextBook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Seller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Purchaser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PurchasePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Profit;
+        private System.Windows.Forms.Button btnCalcProfit;
+    }
+}
+
---
+++ Books Profit CSV/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Books_Profit_CSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void datagridBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        double total = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // setting up open file Dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"C:\\Downloads";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Title = "Open The CSV books file";
+            openFileDialog.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt"; // only allow csv files or text files
+
+            openFileDialog.ShowDialog(); // showing file dialog
+            this.TopMost = true; // bringing form back to the top (I noticed that when you usually use file dialog the form gets minimized)
+            
+            string[] arrReadAllLines = File.ReadAllLines(openFileDialog.FileName);
+            for (int i = 0; i < arrReadAllLines.Length; i++)
+            {
+                List<string> items = arrReadAllLines[i].Split(',').ToList<string>(); // convert to list because you can add to lists after theyre defined
+                string name = items[0];
+                double purchasePrice = double.NaN;
+                double salePrice = double.NaN;
+                double profit = 0;
+                // use tryparse
+                if (Double.TryParse(items[4], out purchasePrice) && Double.TryParse(items[5], out salePrice)) // Checking that both are valid and converting them
+                {
+                    profit = salePrice - purchasePrice;
+                }
+                else
+                {
+                    profit = -purchasePrice;
+                }
+                profit = Math.Round(profit, 1); // round because some times theres a lot of decimal places
+                total += profit; // adds to total profit count because its easier to do it here
+                items.Add(profit.ToString());
+                datagridBooks.Rows.Add(items.ToArray()); // convert back to an array because thats all it takes
+            }
+        }
+
+        private void btnCalcProfit_Click(object sender, EventArgs e)
+        {
+            datagridBooks.Rows[datagridBooks.RowCount - 1].Cells[5].Value = "Total Profit: ";
+            datagridBooks.Rows[datagridBooks.RowCount-1].Cells[6].Value = total.ToString();
+        }
+    }
+}
+
